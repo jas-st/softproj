acddfc6d3d28ef35013b362ced9831a4cc4bfb86
luziab00 <115236076+luziab00@users.noreply.github.com>
2023-01-19 12:11:49 +0100

    some small changes for output

--- complete_proj/simulation.sh
+++ complete_proj/simulation.sh
@@ -8,10 +8,10 @@ k=${4:-1}
 
 echo "This is tree name: $treename";
 
-echo '' > results_raw_$treename.tsv
-
 mkdir -p alignment_alisim;
 cp $treefile alignment_alisim/;
+mkdir -p results_$treename;
+echo '' > results_$treename/results_raw_$treename.tsv;
 
 for value in $(seq $k);
 do
@@ -19,15 +19,14 @@ do
 	cd alignment_alisim;
 	iqtree2 --alisim $treename-alignment -m $matrix -t $1 --length $seq_len > /dev/null;
 	cd ..;
-	./all_tests.out -F alignment_alisim/$treename-alignment.phy >> results_raw_$treename.tsv;
+	./all_tests_new.out -F alignment_alisim/$treename-alignment.phy >> results_$treename/results_raw_$treename.tsv;
 done
 
-mkdir -p plots;
-Rscript analysis.R $treefile $seq_len results_raw_$treename.tsv results_$treename.csv $k > /dev/null; 
+Rscript analysis.R $treefile $seq_len results_$treename/results_raw_$treename.tsv results_$treename/results_$treename.csv $k; 
 
 
 echo "Tree file: " $treefile;
 echo "Length of simulated sequences: " $seq_len;
 echo "Model of substitution: "$matrix;
 echo "Number of simulations: "$k;
-
+echo "Results in directory: results_"$treename; 
