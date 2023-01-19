0e97c672593120fecc4abceaeae0f48476868879
luziab00 <115236076+luziab00@users.noreply.github.com>
2023-01-19 12:11:10 +0100

    some small changes for output

--- complete_proj/analysis.R
+++ complete_proj/analysis.R
@@ -17,65 +17,69 @@ raw_results_file<- args[8]
 results_file<-args[9]
 k<-args[10]
 
+folder<-strsplit(raw_results_file, "/")[[1]][1]
 
 # treename<-"tree.nwk"
 # n<-500
-# k<-2
+# k<-1
 # raw_results<-"results_raw_tree.tsv"
 # results<-"results_tree.csv"
-  
-setwd("/Users/luziaberchtold/Desktop/softproj")
-result_tree <- read_delim(raw_results_file, 
-                          delim = "\t", escape_double = FALSE, 
-                          col_types = cols(...13 = col_skip()), 
-                          trim_ws = TRUE)
+#   
+results_raw_tree <- read_delim(raw_results_file, 
+                              delim = "\t", escape_double = FALSE, 
+                              col_types = cols(`Sat test Cassius 1` = col_number(), 
+                              `Sat test Cassius 2` = col_number(), 
+                              Stuart_test = col_number(), `Internal Symmetry` = col_number(), 
+                              Proposed_test = col_number(), `Chi test` = col_number(), 
+                              Bowker_test = col_number()), trim_ws = TRUE)
 
-result_tree<-result_tree%>%filter(Pair!="Pair")
-result_tree$simulation<-rep(seq(1,k), each=length(unique(result_tree$Pair)))
+result_tree_rename<-results_raw_tree%>%dplyr::rename(Sequences="Sequences compared", Sat_test_Cassius1="Sat test Cassius 1", Sat_test_Cassius2="Sat test Cassius 2", 
+                                                Chi_test="Chi test", Internal_Symmetry="Internal Symmetry")
+
+
+result_tree<-result_tree_rename%>%filter(Sequences!="Sequences compared")
+result_tree$simulation<-rep(seq(1,k), each=length(unique(result_tree$Sequences)))
 
 tree <- read.tree(treename)
 p <- ggtree(tree) + geom_tiplab()
 seq_labels<-unlist(tree["tip.label"])
 
 colnames_ts<-c("Sat test Cassius 1", "Sat test Cassius 2", "Chi test", "Bowker ts", "Stuart ts", "IS ts", "QS ts")
-colnames_nr<-c("Bowker nr", "Stuart nr", "IS nr", "QS nr")
 
-pairs<-strsplit(str_sub(result_tree$Pair, start=2, end=-2), ",")
-pairs<-lapply(pairs, strtoi)
+pairs<-strsplit(str_sub(result_tree$Sequences, start=2, end=-2), ",")
 pairs<-lapply(pairs, unlist)
 pair_1<-unlist(lapply(pairs,function(x) x[1]))
 pair_2<-unlist(lapply(pairs,function(x) x[2]))
-result_tree$pair_1<-pair_1
-result_tree$pair_2<-pair_2
+pair_1<-gsub(" ", "", pair_1, fixed = TRUE)
+pair_2<-gsub(" ", "", pair_2, fixed = TRUE)
+result_tree$seq_1<-pair_1
+result_tree$seq_2<-pair_2
 
-result_tree<-result_tree%>%mutate_at(colnames_nr, ~factor(.x,levels=c("0", "1")))
-result_tree[, colnames_ts] <- sapply(result_tree[, colnames_ts], as.numeric)
+QS_pv<-2*(1-pnorm(abs(result_tree$Proposed_test),mean=0,sd=1)) #standardnormalverteilt
+Bowker_pv<-pchisq(q=result_tree$Bowker_test, df=6, lower.tail=FALSE)#chi square vert mit df=6 #nicht ganz sicher of two sided oder one sided??????
+Stuart_pv<-pchisq(q=result_tree$Stuart_test, df=3, lower.tail=FALSE)#chi square vert mit df=3
+IS_pv<-pchisq(q=result_tree$Internal_Symmetry, df=3, lower.tail=FALSE)#chi square vert mit df=3
+Sat_cassius1_pv<-pnorm(result_tree$Sat_test_Cassius1, 0, sd=sqrt(3/n), lower.tail=FALSE)
+Sat_cassius2_pv<-pnorm(result_tree$Sat_test_Cassius2, 0, sd=sqrt(3/n), lower.tail=FALSE)
+Chi_Test_pv<-pchisq(q=result_tree$Chi_test, df=9, lower.tail=FALSE)
 
-result_tree_rename<-result_tree%>%dplyr::rename(Sat_test_Cassius1="Sat test Cassius 1", Sat_test_Cassius2="Sat test Cassius 2", 
-                                         Chi_test="Chi test", Bowker_ts="Bowker ts", Stuart_ts="Stuart ts", IS_ts="IS ts", QS_ts="QS ts",
-                                         Bowker_nr="Bowker nr", Stuart_nr="Stuart nr", IS_nr="IS nr", QS_nr="QS nr")
+results_pv_all<-data.frame(Pair=result_tree$Sequences, pair_1, pair_2, Bowker_pv, Bowker_ts=result_tree$Bowker_test,Stuart_pv,Stuart_ts=result_tree$Stuart_test, 
+                                        IS_pv, IS_ts=result_tree$Internal_Symmetry, QS_pv, QS_ts=result_tree$Proposed_test, 
+                                        Sat_cassius1_pv, Sat_test_Cassius1=result_tree$Sat_test_Cassius1, Sat_cassius2_pv, Sat_test_Cassius2=result_tree$Sat_test_Cassius2,
+                                        Chi_Test_pv, Chi_test=result_tree$Chi_test, simulation=result_tree$simulation)
 
-QS_pv<-2*(1-pnorm(abs(result_tree_rename$QS_ts),mean=0,sd=1)) #standardnormalverteilt
-Bowker_pv<-pchisq(q=result_tree_rename$Bowker_ts, df=6, lower.tail=FALSE)#chi square vert mit df=6 #nicht ganz sicher of two sided oder one sided??????
-Stuart_pv<-pchisq(q=result_tree_rename$Stuart_ts, df=3, lower.tail=FALSE)#chi square vert mit df=3
-IS_pv<-pchisq(q=result_tree_rename$IS_ts, df=3, lower.tail=FALSE)#chi square vert mit df=3
-Sat_cassius1_pv<-pnorm(result_tree_rename$Sat_test_Cassius1, 0, sd=sqrt(3/n), lower.tail=FALSE)
-Sat_cassius2_pv<-pnorm(result_tree_rename$Sat_test_Cassius2, 0, sd=sqrt(3/n), lower.tail=FALSE)
-Chi_Test_pv<-pchisq(q=result_tree_rename$Chi_test, df=9, lower.tail=FALSE)
+results_pv<-results_pv_all %>%group_by(Pair)%>%summarise(across(-c(simulation, pair_1, pair_2),mean, na.rm = TRUE))
+results_pv$pair_1<-results_pv_all$pair_1[c(seq(1,length(results_pv$Pair)))]
+results_pv$pair_2<-results_pv_all$pair_2[c(seq(1,length(results_pv$Pair)))]
 
-results_pv_all<-data.frame(Pair=result_tree_rename$Pair, pair_1, pair_2, Bowker_pv, Bowker_ts=result_tree_rename$Bowker_ts,Stuart_pv,Stuart_ts=result_tree_rename$Stuart_ts, 
-                                        IS_pv, IS_ts=result_tree_rename$IS_ts, QS_pv, QS_ts=result_tree_rename$QS_ts, 
-                                        Sat_cassius1_pv, Sat_test_Cassius1=result_tree_rename$Sat_test_Cassius1, Sat_cassius2_pv, Sat_test_Cassius2=result_tree_rename$Sat_test_Cassius2,
-                                        Chi_Test_pv, Chi_test=result_tree_rename$Chi_test, simulation=result_tree_rename$simulation)
-
-results_pv<-results_pv_all %>%group_by(Pair)%>%summarise(across(-simulation,mean, na.rm = TRUE))
 
 heat_success<-function(pair_1, pair_2, test_pv, reject=TRUE, seq_lables=seq_labels){
-  pair_1_tmp<-seq_labels[c(pair_1, pair_2)]
-  pair_2_tmp<-seq_labels[c(pair_2, pair_1)]
+  pair_1_tmp<-c(pair_1, pair_2)
+  pair_2_tmp<-c(pair_2, pair_1)
   test_pv<-rep(test_pv,2)
   
   test<-data.frame(pair_1=factor(pair_1_tmp),pair_2=factor(pair_2_tmp),test_pv=test_pv)
+  
   test<-test%>%mutate(test_rej=factor(ifelse(test_pv>=0.05, 0, 1), levels=c(0,1), ordered=TRUE))
   #shape_values<-ifelse(reject==TRUE, c(1,4), c(4,1))
   test%>%ggplot(aes(x=pair_1, y=pair_2))+geom_tile(aes(fill=test_pv))+
@@ -86,7 +90,9 @@ heat_success<-function(pair_1, pair_2, test_pv, reject=TRUE, seq_lables=seq_labe
 }
 
 colored_tree <- function(tree, pair_1, pair_2, test_pv){
-  paths<-mapply(function(x,y) get.path(tree, x,y), pair_1, pair_2)
+  pair_1_index<-mapply(function(x) which(x==seq_labels), pair_1)
+  pair_2_index<-mapply(function(x) which(x==seq_labels), pair_2)
+  paths<-mapply(function(x,y) get.path(tree, x,y), pair_1_index, pair_2_index)
   rej_paths<-mapply(function(x) ifelse(x>=0.05,0,1), test_pv )
   
   edges<-sort(unique(unlist(paths)))
@@ -109,7 +115,7 @@ colored_tree <- function(tree, pair_1, pair_2, test_pv){
 }
 
 
-pdf("plots/plot_Bowker_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_Bowker_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$Bowker_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$Bowker_pv, reject=TRUE)
@@ -117,7 +123,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("Bowker Test", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_Stuart_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_Stuart_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$Stuart_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$Stuart_pv, reject=TRUE)
@@ -125,7 +131,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("Stuart Test", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_IS_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_IS_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$IS_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$IS_pv, reject=TRUE)
@@ -133,7 +139,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("IS Test", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_QS_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_QS_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$QS_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$QS_pv, reject=TRUE)
@@ -141,7 +147,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("QS Test", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_Sat_Cassius1_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_Sat_Cassius1_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$Sat_cassius1_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$Sat_cassius1_pv, reject=TRUE)
@@ -149,7 +155,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("Saturation Test Cassius 1", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_Sat_Cassius2_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_Sat_Cassius2_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$Sat_cassius2_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$Sat_cassius2_pv, reject=TRUE)
@@ -157,7 +163,7 @@ p<-ggarrange(
 annotate_figure(p,top=text_grob("Saturation Test Cassius 2", face = "bold", size = 14))
 dev.off()
 
-pdf("plots/plot_Chi_test.pdf",width = 13, height = 7)
+pdf(paste(folder, "plot_Chi_test.pdf", sep="/"),width = 13, height = 7)
 p<-ggarrange(
   colored_tree(tree, results_pv$pair_1, results_pv$pair_2, results_pv$Chi_Test_pv),
   heat_success(results_pv$pair_1, results_pv$pair_2, results_pv$Chi_Test_pv, reject=TRUE)
@@ -166,6 +172,28 @@ annotate_figure(p,top=text_grob("Chi Test", face = "bold", size = 14))
 dev.off()
 
 
-write.csv(results_pv_all, results_file)
+write.csv(results_pv_all, results_file, row.names = FALSE)
+
+
+#test failed/success
+
+Bowker_success<-mapply(function(x) ifelse(x>=0.05,"success","failed"), Bowker_pv)
+Stuart_success<-mapply(function(x) ifelse(x>=0.05,"success","failed"), Stuart_pv)
+IS_success<-mapply(function(x) ifelse(x>=0.05,"success","failed"), IS_pv)
+QS_success<-mapply(function(x) ifelse(x>=0.05,"success","failed"), QS_pv)
+Reversibility_test<-data.frame(Pair=results_pv_all$Pair, Simulation=results_pv_all$simulation,
+                               Bowker_success, Stuart_success, IS_success, QS_success)
+
+write.csv(Reversibility_test, paste(folder, "results_rev_test.csv", sep="/"),row.names = FALSE)
+
+Sat_cassius1_success<-mapply(function(x) ifelse(x>=0.05,"failed", "success"), Sat_cassius1_pv)
+Sat_cassius2_success<-mapply(function(x) ifelse(x>=0.05,"failed", "success"), Sat_cassius2_pv)
+Chi_success<-mapply(function(x) ifelse(x>=0.05,"failed", "success"), Chi_Test_pv)
+Saturation_test<-data.frame(Pair=results_pv_all$Pair, Simulation=results_pv_all$simulation,
+                            Sat_cassius1_success, Sat_cassius2_success, Chi_success)
+
+write.csv(Saturation_test, paste(folder, "results_sat_test.csv", sep="/"),row.names = FALSE)
+
+
 sink(type="message")
 close(zz)
