56f163a4d2830e00362f2476fb0b4a4468e6e6b6
Zhasmina Stoyanova <84456827+jas-st@users.noreply.github.com>
2023-02-16 13:23:16 +0100

    Update Sequence.h

--- complete_proj/lib/Sequence.h
+++ complete_proj/lib/Sequence.h
@@ -13,18 +13,14 @@ class Sequence {
 
         //nucleotide frequencies for the specific sequence {A,C,G,T}
         Eigen::Vector4d nucl_freqs = Eigen::Vector4d::Zero();
+    
+        //nucleotide frequncies only looking at the alignment {A,C,G,T}
+        Eigen::Vector4d nucl_freqs_al = Eigen::Vector4d::Zero();
 
         Sequence(std::string seq_id, std::string seq_str, Eigen::Vector4d seq_freq) {
             id = seq_id;
             seq = seq_str;
             length = seq_freq.sum();
-            nucl_freqs = seq_freq;
-        }
-
-        Sequence(std::string seq_str, Eigen::Vector4d seq_freq) {
-            //id = "none";
-            seq = seq_str;
-            //length = seq_freq.sum();
             nucl_freqs = seq_freq;
         }
 
